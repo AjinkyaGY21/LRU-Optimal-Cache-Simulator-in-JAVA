02b86ede90e4b5e4f72e208d2281a63a658cd766
Ajinkya Yawalkar <136622844+AjinkyaGY21@users.noreply.github.com>
2024-09-14 15:18:08 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,20 +1,15 @@
-public class Main{
+public class Main {
     public static void main(String[] args) {
-
-        // Testing LRU cache
+        // Testing LRU Cache
         LRU_Cache lruCache = new LRU_Cache(3);
-        int[] pg = {10,30,60,40,20,10,30,50,70,90,60,20,10,30,60,50};
-        int fn = 4;
-        lruCache.lruPage(pg, fn);
+        int[] pg = {10, 30, 60, 40, 20, 10, 30, 50, 70, 90, 60, 20, 10, 30, 60, 50};
+        lruCache.lruPage(pg);
 
         System.out.println();
 
-        // Testing Optimal cache
+        // Testing Optimal Cache
         Optimal_Cache optimalCache = new Optimal_Cache();
-        int[] seq = {10,30,60,40,20,10,30,50,70,90,60,20,10,30,60,50};
-        int pn = seq.length;
-        int fr = 3;
-        optimalCache.optimalPage(seq, pn, fr);
-
+        int pn = pg.length;
+        optimalCache.optimalPage(pg, pn, 3);
     }
 }
